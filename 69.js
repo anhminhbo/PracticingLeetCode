abae5a8f00c150acb2f47a888ae1bf890afab506
Minh Nguyens <minhnguyen@wee.vn>
2022-06-23 13:57:55 +0700

    fix leetcode 69

--- 69.js
+++ 69.js
@@ -1,6 +1,3 @@
-// 69. Sqrt(x)
-// https://leetcode.com/problems/sqrtx/
-
 /**
  * @param {number} x
  * @return {number}
@@ -9,12 +6,25 @@ var mySqrt = function (x) {
   // 1,2,3,4,5,6,7,8,9
   // 1,4,9,16,25,36,49,64,81
 
-  if (x === 0) return 0;
-  if (x === 1) return 1;
+  if (x < 2) return x;
 
-  for (let i = 0; i <= x; i++) {
-    let sqr = i * i;
-    if (x === sqr) return i;
-    if (x < sqr) return --i;
+  //     for (let i = 0; i <= x; i++) {
+  //         let sqr = i * i
+  //         if (x === sqr) return i;
+  //         if (x < sqr) return --i
+  //     }
+
+  var left = 1;
+  var right = x;
+  var mid = 0;
+  while (left <= right) {
+    mid = left + Math.floor((right - left) / 2);
+    if (mid > x / mid) {
+      right = mid - 1;
+    } else if (mid + 1 > x / (mid + 1)) {
+      return mid;
+    } else {
+      left = mid + 1;
+    }
   }
 };

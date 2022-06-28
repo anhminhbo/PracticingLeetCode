bceb0d950b250f8c67af0a8020fe44be14d07645
Minh Nguyens <minhnguyen@wee.vn>
2022-06-28 23:29:51 +0700

    update leetcode 121 modified

--- 121.java
+++ 121.java
@@ -17,39 +17,41 @@ class Solution {
         // return maxPro;
 
         int maxPro = 0;
+        if (prices.length <= 1) {
+            return maxPro;
+        }
 
-        int buy = -1;
-        int sell = -1;
+        int buy = prices[0];
+        int sell = 0;
 
-        for (int i = 0; i < prices.length; i++) {
-            // check if has not buy yet
-            if (buy == -1) {
-                if (i == prices.length - 1)
-                    return maxPro;
+        for (int i = 1; i < prices.length; i++) {
+            // // check if has not buy yet
+            // if (buy == -1) {
+            // if (i == prices.length - 1) return maxPro;
 
-                buy = prices[i];
-                sell = prices[i + 1];
+            // buy = prices[i];
+            // sell = prices[i+1];
 
-                // check if maxPro is not good --> not buying this
-                if (maxPro < sell - buy) {
-                    maxPro = sell - buy;
-                } else {
-                    buy = -1;
-                }
+            // // check if maxPro is not good --> not buying this
+            // if (maxPro < sell - buy) {
+            // maxPro = sell - buy;
+            // } else {
+            // buy = -1;
+            // }
 
-                // if already buy now we sell
-            } else {
-                sell = prices[i];
+            // // if already buy now we sell
+            // } else {
+            sell = prices[i];
 
-                if (maxPro < sell - buy) {
-                    maxPro = sell - buy;
-                }
-
-                if (buy > sell) {
-                    buy = sell;
-                }
-
+            if (maxPro < sell - buy) {
+                maxPro = sell - buy;
             }
+
+            if (buy > sell) {
+                buy = sell;
+            }
+
+            // }
 
         }
 
